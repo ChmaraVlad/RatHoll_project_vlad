68c761ddf65c1870a0cf8e0b9beab9ef2b8f894e
Chmara Vlad <chmaravlad5@gmail.com>
2022-05-02 14:19:56 +0300

    Fix all logic in keyPadRuss componentand add
    keysLowerComp and UppercaseComp

---
+++ src/view/components/KeyPadRuss/KeysUppercase/index.tsx
@@ -0,0 +1,87 @@
+
+// Core
+import React, { FC } from 'react';
+
+// Component
+import { ruKeyboard } from '../../../../tools/utils/keyboardData';
+
+// Bus
+// import {} from '../../../bus/'
+import { TogglersKeys } from '../../../../bus/client/togglers';
+
+
+// Types
+type PropTypes = {
+    /* type props here */
+    setMsg: React.Dispatch<React.SetStateAction<string>>,
+    handleSubmit: (event: React.MouseEvent | React.FormEvent<HTMLFormElement>) => void;
+    msg: string;
+    setTogglerAction: (options: Options) => undefined
+    isCapitalize: boolean
+}
+type Options = { type: TogglersKeys, value: boolean };
+type clickTypeEvent = React.MouseEvent | React.FormEvent<HTMLFormElement>
+
+
+export const KeysUppercase: FC<PropTypes> = ({ msg, setMsg, handleSubmit, setTogglerAction, isCapitalize }) => {
+    return (
+        <>
+            {
+                ruKeyboard[ 1 ].map((item, indexRow)=>{
+                    const row = item;
+
+                    const handleClick = (event: clickTypeEvent, key: string) => {
+                        if (key === 'En') {
+                            setTogglerAction({ type: 'isEnglKeyPad', value: true });
+                        } else if (key === 'Enter') {
+                            handleSubmit(event);
+                            console.log('enteer');
+                        } else if (key === 'Backspace') {
+                            setMsg(msg.slice(0, -1));
+                        } else if (key === 'Space') {
+                            setMsg(msg + ' ');
+                        } else if (key === 'Shift') {
+                            setTogglerAction({ type: 'isCapitalize', value: !isCapitalize });
+                        } else {
+                            setMsg(msg + key);
+                        }
+                        console.log(key);
+                    };
+
+                    let clRow = indexRow === 0 ? 'keyboard__row' : 'keyboard__row keyboard__row--ru';
+                    if (indexRow === 0) {
+                        clRow = 'keyboard__row';
+                    } else if (indexRow === 1) {
+                        clRow = 'keyboard__row keyboard__row--ru';
+                    } else if (indexRow === 2 || indexRow === 3) {
+                        clRow = 'keyboard__row keyboard__row--ru-small';
+                    } else if (indexRow === 4) {
+                        clRow = 'keyboard__row keyboard__row--footer';
+                    }
+
+                    return (
+                        <div
+                            className = { clRow }
+                            key = { indexRow }>
+                            {
+                                row.map((btn, indexBtn)=>{
+                                    const { key, code } = btn;
+
+                                    return (
+                                        <button
+                                            className = 'keyboard__item'
+                                            key = { indexBtn }
+                                            onClick = {
+                                                (event: clickTypeEvent)=> handleClick(event, key) }>{key
+                                            }
+                                        </button>
+                                    );
+                                })
+                            }
+                        </div>
+                    );
+                })
+            }
+        </>
+    );
+};
--- src/view/components/KeyPadRuss/index.tsx
+++ src/view/components/KeyPadRuss/index.tsx
@@ -1,8 +1,10 @@
 // Core
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 
 // Bus
 import { useTogglersRedux } from '../../../bus/client/togglers';
+import { KeysLowercase } from './KeysLowercase';
+import { KeysUppercase } from './KeysUppercase';
 
 // Styles
 import * as S from './styles';
@@ -10,82 +12,42 @@ import * as S from './styles';
 // Types
 type PropTypes = {
     /* type props here */
+    setMsg: React.Dispatch<React.SetStateAction<string>>,
+    handleSubmit: (event: React.MouseEvent | React.FormEvent<HTMLFormElement>) => void;
+    msg: string;
 }
+type clickTypeEvent = React.MouseEvent | React.FormEvent<HTMLFormElement>
+// type clickTypeEvent =  React.FormEvent<HTMLFormElement>
 
-export const KeyPadRuss: FC<PropTypes> = () => {
-    const { setTogglerAction } = useTogglersRedux();
+export const KeyPadRuss: FC<PropTypes> = ({ msg, setMsg, handleSubmit }) => {
+    const { setTogglerAction, togglersRedux: { isCapitalize }} = useTogglersRedux();
 
-    const handleKeyPad = () => {
-        setTogglerAction({ type: 'isEnglKeyPad', value: true });
-    };
+    const isLowercase =  isCapitalize ? (
+        <KeysUppercase
+            handleSubmit = { handleSubmit }
+            isCapitalize = { isCapitalize }
+            msg = { msg }
+            setMsg = { setMsg }
+            setTogglerAction = { setTogglerAction }
+        />
+    ) : (
+        <KeysLowercase
+            handleSubmit = { handleSubmit }
+            isCapitalize = { isCapitalize }
+            msg = { msg }
+            setMsg = { setMsg }
+            setTogglerAction = { setTogglerAction }
+        />
+    );
 
     return (
         <S.Container>
-            <div className = 'keyboard keyboard--ru '>
+            <div className = 'keyboard keyboard--ru'>
                 <div className = 'keyboard__container'>
                     <div className = 'keyboard__body'>
-                        <div className = 'keyboard__row'>
-                            <div className = 'keyboard__item'>1</div>
-                            <div className = 'keyboard__item'>2</div>
-                            <div className = 'keyboard__item'>3</div>
-                            <div className = 'keyboard__item'>4</div>
-                            <div className = 'keyboard__item'>5</div>
-                            <div className = 'keyboard__item'>6</div>
-                            <div className = 'keyboard__item'>7</div>
-                            <div className = 'keyboard__item'>8</div>
-                            <div className = 'keyboard__item'>9</div>
-                            <div className = 'keyboard__item'>0</div>
-                        </div>
-                        <div className = 'keyboard__row keyboard__row--ru'>
-                            <div className = 'keyboard__item letter'>й</div>
-                            <div className = 'keyboard__item letter'>ц</div>
-                            <div className = 'keyboard__item letter'>у</div>
-                            <div className = 'keyboard__item letter'>к</div>
-                            <div className = 'keyboard__item letter'>е</div>
-                            <div className = 'keyboard__item letter'>н</div>
-                            <div className = 'keyboard__item letter'>г</div>
-                            <div className = 'keyboard__item letter'>ш</div>
-                            <div className = 'keyboard__item letter'>щ</div>
-                            <div className = 'keyboard__item letter'>з</div>
-                            <div className = 'keyboard__item letter'>х</div>
-                            <div className = 'keyboard__item letter'>ъ</div>
-                        </div>
-                        <div className = 'keyboard__row  keyboard__row--ru-small'>
-                            <div className = 'keyboard__item letter'>ф</div>
-                            <div className = 'keyboard__item letter'>ы</div>
-                            <div className = 'keyboard__item letter'>в</div>
-                            <div className = 'keyboard__item letter'>а</div>
-                            <div className = 'keyboard__item letter'>п</div>
-                            <div className = 'keyboard__item letter'>р</div>
-                            <div className = 'keyboard__item letter'>о</div>
-                            <div className = 'keyboard__item letter'>л</div>
-                            <div className = 'keyboard__item letter'>д</div>
-                            <div className = 'keyboard__item letter'>ж</div>
-                            <div className = 'keyboard__item letter'>э</div>
-                        </div>
-                        <div className = 'keyboard__row  keyboard__row--ru-small'>
-                            <div className = 'keyboard__item'>Shift</div>
-                            <div className = 'keyboard__item letter'>я</div>
-                            <div className = 'keyboard__item letter'>ч</div>
-                            <div className = 'keyboard__item letter'>с</div>
-                            <div className = 'keyboard__item letter'>м</div>
-                            <div className = 'keyboard__item letter'>и</div>
-                            <div className = 'keyboard__item letter'>т</div>
-                            <div className = 'keyboard__item letter'>ь</div>
-                            <div className = 'keyboard__item letter'>б</div>
-                            <div className = 'keyboard__item letter'>ю</div>
-                            <div className = 'keyboard__item'>Backspace</div>
-                        </div>
-                        <div className = 'keyboard__row keyboard__row--footer'>
-                            <div className = 'keyboard__item'>,</div>
-                            <div
-                                className = 'keyboard__item'
-                                onClick = { handleKeyPad }>Ru
-                            </div>
-                            <div className = 'keyboard__item'>Space</div>
-                            <div className = 'keyboard__item'>.</div>
-                            <div className = 'keyboard__item'>Enter</div>
-                        </div>
+                        {
+                            isLowercase
+                        }
                     </div>
                 </div>
             </div>
