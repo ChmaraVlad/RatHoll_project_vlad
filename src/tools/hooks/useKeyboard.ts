492e42c9b253d14d7cfafed5f4da1113e68ed3b0
Chmara Vlad <chmaravlad5@gmail.com>
2022-05-03 21:04:30 +0300

    Fix useKeyboardHook and keyboardData

--- src/tools/hooks/useKeyboard.ts
+++ src/tools/hooks/useKeyboard.ts
@@ -9,28 +9,33 @@ import { ruKeyboard, englKeyboard } from '../utils/keyboardData';
 import { useDispatch } from 'react-redux';
 import { keyboardActions } from '../../bus/keyboard/slice';
 import { useTogglersRedux } from '../../bus/client/togglers';
+import { useInputMessage } from '../../bus/inputMessage';
 
 export const useKeyboardHook = () => {
     const keys = useSelector((state) => state.keyboard.keys);
     const activeKeys = useSelector((state) => state.keyboard.activeKeys);
+
+    const { onClickWriteMessage, deleteOneLetterMessage, sendMessage } = useInputMessage();
 
     const { togglersRedux:{ isShowKeyPad, isEnglKeyPad, isCapitalize }, setTogglerAction } = useTogglersRedux();
 
     const dipatch = useDispatch();
 
     useEffect(()=>{
-        window.addEventListener('keydown', (event: KeyboardEvent) => {
+        document.addEventListener('keydown', (event: KeyboardEvent) => {
             if (event.key === 'Shift') {
                 setTogglerAction({ type: 'isCapitalize', value: !isCapitalize });
             }
+
             dipatch(keyboardActions.addActiveKey(event.key));
         });
 
-        window.addEventListener('keyup', (event: KeyboardEvent) => {
+        document.addEventListener('keyup', (event: KeyboardEvent) => {
             dipatch(keyboardActions.removeActiveKey(event.key));
         });
     }, []);
 
+    // --------- layout basic logic
     const registerLayoutEngl = isCapitalize ? englKeyboard[ 1 ] : englKeyboard[ 0 ];
     const registerLayoutRu = isCapitalize ? ruKeyboard[ 1 ] : ruKeyboard[ 0 ];
 
@@ -42,6 +47,32 @@ export const useKeyboardHook = () => {
         keys,
         activeKeys,
         isShowKeyPad,
+        setTogglerAction,
         useLayout,
+
+        changeText: (key: string) => {
+            switch (key) {
+                case 'Shift':
+                    setTogglerAction({ type: 'isCapitalize', value: !isCapitalize });
+                    break;
+                case 'Backspace':
+                    deleteOneLetterMessage();
+                    break;
+                case 'En':
+                    setTogglerAction({ type: 'isEnglKeyPad', value: !isEnglKeyPad });
+                    break;
+                case 'Ru':
+                    setTogglerAction({ type: 'isEnglKeyPad', value: !isEnglKeyPad });
+                    break;
+                case 'Space':
+                    onClickWriteMessage(' ');
+                    break;
+                case 'Enter':
+                    sendMessage();
+                    break;
+                default:
+                    onClickWriteMessage(key);
+            }
+        },
     };
 };
